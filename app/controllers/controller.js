913f0cb71cece22d28c541bede216e1fd1fc8a07
Jlens3 <155668446+Jlens3@users.noreply.github.com>
2024-02-09 13:24:26 -0500

    Update controller.js

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -19,8 +19,7 @@ exports.login = async (req, res) => {
         };
 
     const userAgent = req.headers["user-agent"];
-    const systemLang = req.headers["accept-language"];
-
+    
     const ipAddress = getClientIp(req);
     
     try {
